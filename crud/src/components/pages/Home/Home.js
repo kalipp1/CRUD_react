75123566844cda3a23319ec37d2c155ba00d979c
Maks Birecki <maksymilian.birecki@gmail.com>
2024-10-03 17:17:52 +0200

    addAllPostsRendering

--- crud/src/components/pages/Home/Home.js
+++ crud/src/components/pages/Home/Home.js
@@ -1,10 +1,27 @@
 import styles from './Home.module.scss';
+import { useSelector } from 'react-redux';
+import { getAllPosts } from '../../../redux/postsRedux';
 
 const Home = () => {
+    const posts = useSelector(getAllPosts);
+
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+        <div>
+            <h1>All posts</h1>
+            <section className={styles.posts}>
+                {posts.map(post => 
+                    <article className={styles.post}>
+                        <h4>{post.title}</h4>
+                        <div className={styles.info}>
+                        <span className={styles.infoItem}><b>Author: </b><p>{post.author}</p></span>
+                        <span className={styles.infoItem}><b>Published: </b><p>{post.publishedDate}</p></span>
+                        </div>
+                        <p>{post.shortDescription}</p>
+                        <button className={styles.button}>Read more</button>
+                    </article>
+                )}
+            </section>
+        </div>
     );
   };
 
--- crud/src/redux/initialState.js
+++ crud/src/redux/initialState.js
@@ -1,5 +1,30 @@
 const initialState = {
-    posts: []
+    posts: [
+        {
+            id: '1',
+            title: 'Cooking',
+            shortDescription: 'Cooking dishes ...',
+            content: 'Food',
+            publishedDate: '02-02-2022',
+            author: 'John Doe'
+      },
+      {
+            id: '2',
+            title: 'Listening',
+            shortDescription: 'Listening to music ...',
+            content: 'Music',
+            publishedDate: '13-08-2023',
+            author: 'David Copper'
+      },
+      {
+            id: '3',
+            title: 'Watching',
+            shortDescription: 'Watching movies ...',
+            content: 'Movies',
+            publishedDate: '29-11-2024',
+            author: 'Gustav Field'
+      }
+    ]
 };
 
 export default initialState;
